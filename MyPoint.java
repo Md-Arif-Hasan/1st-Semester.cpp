49d0121a0a826a7cfe383f77b59755350a861c76
Md-Arif-Hasan <48629132+Md-Arif-Hasan@users.noreply.github.com>
2020-01-12 22:19:42 +0600

    Add files via upload

---
+++ MyPoint.java
@@ -0,0 +1,13 @@
+package SystemOfShape;
+
+
+public class MyPoint {
+    public int x;
+    public int y;
+    public MyPoint(int x,int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+
