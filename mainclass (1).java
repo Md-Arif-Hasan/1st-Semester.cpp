de8936b5182d47ec74e4a1634f14ed2a557fc01f
Md Arif Hasan <48629132+Md-Arif-Hasan@users.noreply.github.com>
2020-01-28 07:30:26 +0600

    Add files via upload

---
+++ mainclass (1).java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Account_System;
+
+/**
+ *
+ * @author User
+ */
+public class mainclass {
+    public static void main(String[] args) {
+        Library iit_library = new Library();
+        iit_library.initialize();
+        
+        iit_library.search();
+    }
+}
