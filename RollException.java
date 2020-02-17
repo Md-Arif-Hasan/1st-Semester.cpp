e7d444a8955389156381ea0f87f920908f25bddd
Md Arif Hasan <48629132+Md-Arif-Hasan@users.noreply.github.com>
2020-02-18 03:41:24 +0600

    Exception

---
+++ RollException.java
@@ -0,0 +1,14 @@
+
+package Exception_Handling;
+
+/**
+ *
+ * @author Arif Hasan
+ */
+public class RollException extends Exception{
+    public RollException( String message)
+    {
+        super(message);
+    }
+    
+}
