f5f9c11b3633adf177179d0c2736804f847db77f
Md Arif Hasan <48629132+Md-Arif-Hasan@users.noreply.github.com>
2020-02-18 16:33:24 +0600

    Data Storing

---
+++ BufferdReader.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data_Storing;
+
+import java.io.FileReader;
+
+/**
+ *
+ * @author Arif Hasan
+ */
+class BufferdReader {
+
+    BufferdReader(FileReader fr) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String readLine() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void close() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
